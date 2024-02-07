15c9a86f859390486a719fcf60902edc472ae672
MukwevhoDN <mukwevhoneville59@gmail.com>
2024-02-07 21:03:03 +0200

    try

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import Message from  "./Log";
-import Welcome from "./welcome";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importing Routes instead of Switch
+import Message from './Log';
+import Welcome from './welcome';
 
-function App(){
-  return <div><Welcome/></div>;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/about" element={<Message />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
-  
